844c50a908816e37a3a8bf1529cb0434793c740e
fahrulriski0201 <101050082+fahrulriski0201@users.noreply.github.com>
2022-03-07 20:52:33 +0800

    day2_Java_Stdin_and_Stdout.java

---
+++ day2_Java_Stdin_and_Stdout.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int x = scan.nextInt();
+        int d = scan.nextInt();
+        // Write your code here.
+
+        System.out.println(a);
+        System.out.println(x);
+        System.out.println(d);
+    }
+}
